d12d57bdf9db889487db94e8fe5d6f5208dcf6af
thissak <thissak@gmail.com>
2022-08-29 18:31:27 +0900

    add @comboBox delete @listWidget

--- test.py
+++ test.py
@@ -11,9 +11,6 @@ from PyQt5 import uic
 form_class = uic.loadUiType("test.ui")[0]
 
 
-# hz = 30.00
-
-
 # 화면을 띄우는데 사용되는 Class 선언
 class WindowClass(QDialog, form_class):
 
@@ -21,26 +18,28 @@ class WindowClass(QDialog, form_class):
         super().__init__()
         self.setupUi(self)
 
-        self.chk1_sync.stateChanged.connect(self.chk_btn_fuc)
-        self.btn1_mosaic.clicked.connect(self.btn1_mosaic_fnc)
-        self.btn2_clear.clicked.connect(self.btn2_clear_fnc)
-        self.btn3_current.clicked.connect(self.bnt3_current_fnc)
-        self.list1_hz.itemClicked.connect(self.chkItemClicked)
-        # 현재 LISTWIDGET을 0번째 줄로 설정
-        self.list1_hz.setCurrentRow(0)
+        self.chk1_sync.stateChanged.connect(self.chk_btn_func)
+        self.btn1_mosaic.clicked.connect(self.btn1_mosaic_func)
+        self.btn2_clear.clicked.connect(self.btn2_clear_func)
+        self.btn3_current.clicked.connect(self.bnt3_current_func)
+        self.btn4_sync.clicked.connect(self.btn4_sync_func)
+        self.combo_hz.currentIndexChanged.connect(self.combo_hz_func)
+        self.combo_sync.currentIndexChanged.connect(self.combo_sync_func)
 
+    ###############################################
     # FUNTIONS #
+    ###############################################
 
-    # 모자이크 활성화 RETURN XML(xml), XMLSTRING(str)
+    # 모자이크 활성화 RETURN[xml, str]
     def enableMosaic(self):
-        hz = self.chkItemClicked()
+        hz = self.combo_hz_func()
         f = "C:\\configureMosaic.exe test rows=1 cols=1 res=2560,1600,{0} gridPos=0,0 out=0,0 rotate=0  nextgrid " \
             "rows=1 cols=1 res=3840,2160,{0} gridPos=2560,0 out=0,1 rotate=0".format(hz, hz)
         xmlString = os.popen(f).read()
         xml = ET.fromstring(xmlString)
         return xml, xmlString
 
-    # listconfigcmd 모자이크 체크 RETURN bool. firstgrid(list), nextgrid(list)
+    # 모자이크 체크 RETURN[bool, list, list]
     @staticmethod
     def isMosaiced():
         cmd = os.popen("c:\\configureMosaic.exe listconfigcmd").read().split(" ")
@@ -57,23 +56,37 @@ class WindowClass(QDialog, form_class):
 
         return isMosaiced, firstgrid, nextgrid
 
-    # BUTTONS #
+    ###############################################
+    # BUTTON'S FUNCTIONS#
+    ###############################################
 
-    # LISTWIDGET FUNTION RETURN hz(float)
+    # COMBOBOX FUNCTION
+    def combo_hz_func(self):
+        hz = float(self.combo_hz.currentText()[:-2])
+        return hz
 
-    def chkItemClicked(self):
-        hz = self.list1_hz.currentItem().text()
-        return float(hz[:-2])
+    def combo_sync_func(self):
+        current = self.combo_sync.currentText()
+        self.label1.setText("Sync parameter set " + current)
+        return current
 
     # CHECKBOX FUNCTION
-    def chk_btn_fuc(self):
-        if self.chk_1.isChecked():
+    def chk_btn_func(self):
+        if self.chk1_sync.isChecked():
             self.label1.setText("모자이크 활성화에 이어서 싱크작업을 진행합니다.")
-        if not self.chk_1.isChecked():
+            self.btn4_sync.setEnabled(False)
+        if not self.chk1_sync.isChecked():
             self.label1.setText("모자이크 활성화만 진행합니다.")
+            self.btn4_sync.setEnabled(True)
 
-    # CURRENT BUTTON FUNCTION
-    def bnt3_current_fnc(self):
+    # BUTTON FUNCTION ###
+    # SYNC_BUTTON
+    def btn4_sync_func(self):
+        current = self.combo_sync_func()
+        self.label1.setText(current + " sync 활성화를 진행합니다.")
+
+    # CURRENT_BUTTON
+    def bnt3_current_func(self):
         self.textLog.clear()
         self.label1.setText("현재 상태입니다.")
         isMosaiced, firstgrid, nextgrid = WindowClass.isMosaiced()
@@ -86,27 +99,28 @@ class WindowClass(QDialog, form_class):
             self.textLog.appendPlainText(str(firstgrid))
             self.textLog.appendPlainText(str(nextgrid))
 
-    # CLEAR BUTTON FUNCTION
-    def btn2_clear_fnc(self):
+    # CLEAR_BUTTON
+    def btn2_clear_func(self):
         self.textLog.clear()
-        self.label1.setText("")
+        self.label1.setText("log is cleared!")
 
-    # MOSAIC BUTTON FUNCTION
-    def btn1_mosaic_fnc(self):
+    # MOSAIC_BUTTON
+    def btn1_mosaic_func(self):
         self.textLog.clear()
         xml, xmlString = self.enableMosaic()
-        # isMosaiced, firstgrid, nextgrid = WindowClass.isMosaiced()
-        # self.textLog.appendPlainText("현재 모자이크 상태는 " + str(isMosaiced) + " 입니다.")
-        # self.textLog.appendPlainText(str(firstgrid))
-        # self.textLog.appendPlainText(str(nextgrid))
         self.textLog.appendPlainText(xmlString)
+        hz = self.combo_hz_func()
 
         if xml.attrib['valid'] == "1":
-            self.label1.setText("모자이크가 활성화 되었습니다.")
+            self.label1.setText("{0}hz 모자이크가 활성화 되었습니다.".format(hz))
         else:
-            self.bnt3_current_fnc()
+            self.bnt3_current_func()
             self.label1.setText("모자이크가 활성화에 실패했습니다.")
 
+
+###############################################
+# main #
+###############################################
 
 if __name__ == "__main__":
     # QApplication : 프로그램을 실행시켜주는 클래스
